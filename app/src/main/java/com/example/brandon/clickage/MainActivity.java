c4d4a48f46416071cd9c969b830ab12cc1062eb2
Brandon Baksh <bbaksh@uwo.ca>
2015-09-16 17:56:59 -0400

    pause button functionality added and runtime highscores added

--- app/src/main/java/com/example/brandon/clickage/MainActivity.java
+++ app/src/main/java/com/example/brandon/clickage/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -16,42 +17,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public RelativeLayout screen;
     public TextView t;
     public TextView score;
+    public TextView highScoreDisplay;
     public ProgressBar timer;
     public CountDownTimer countDown;
+    public Button pauseBut;
     public int time = 0;
     public int x = 0;
+    public int highScore = 0;
+    public int timeRemaining = 15000;
+    public boolean isPaused=false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //setup screen
-        screen = (RelativeLayout)findViewById(R.id.main);
-
-        //set up main text view for tapping counter
-        t = (TextView)findViewById(R.id.editText);
-        t.setOnClickListener(this);
-        t.setText("Tap as fast as you can in 15 seconds");
-        t.setTextSize(20);
-        score = (TextView)findViewById(R.id.textView);
-        score.setText("");
-
-
-        //set up timer for progress bar
-        timer = (ProgressBar)findViewById(R.id.progressBar);
-        timer.setProgress(time);
-        countDown = new CountDownTimer(15000,1000){
+    public void timeActivation() {
+        countDown = new CountDownTimer(timeRemaining, 500) {
             @Override
             public void onTick(long millisUntilFinished) {
+                timeRemaining = (int)millisUntilFinished;
                 time++;
-                timer.setProgress(time);
+                timer.setProgress(time/2);
             }
 
             @Override
             public void onFinish() {
-                countDown.cancel();
                 time++;
-                timer.setProgress(time);
+                timer.setProgress(time/2);
+                if (x > highScore) {
+                    highScore = x;
+                    highScoreDisplay.setText("Highscore: " + String.valueOf(highScore));
+                }
                 if (x > 0 && x <= 50) {
                     score.setText(String.valueOf(x));
                     score.setTextSize(80);
@@ -77,7 +69,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     t.setTextSize(25);
                 }
             }
-        };
+        }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setup screen
+        screen = (RelativeLayout)findViewById(R.id.main);
+
+        //setup buttons
+        pauseBut = (Button)findViewById(R.id.button2);
+
+        //set up main text views for tapping counter
+        t = (TextView)findViewById(R.id.editText);
+        t.setOnClickListener(this);
+        t.setText("Tap as fast as you can in 15 seconds");
+        t.setTextSize(20);
+        score = (TextView)findViewById(R.id.textView);
+        score.setText("");
+        highScoreDisplay = (TextView)findViewById(R.id.highScoreText);
+        highScoreDisplay.setText("");
+
+        //set up timer for progress bar
+        timer = (ProgressBar)findViewById(R.id.progressBar);
+        timer.setProgress(time);
     }
 
     @Override
@@ -103,24 +120,49 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void onClick(View view){
-        if(time>=15){
+        if(time>=30 || isPaused==true){
             return;
         }
         ++x;
         if(x==1) {
-            countDown.start();
-
+            timeActivation();
         }
         t.setText(String.valueOf(x));
         t.setTextSize(100);
+        if(x>highScore){
+            highScore=x;
+            highScoreDisplay.setText("Highscore: " + String.valueOf(highScore));
+        }
     }
     public void restart(View view){
+        if(x>0){
+            countDown.cancel();
+        }
         x=0;
         time=0;
-        countDown.cancel();
+        timeRemaining=15000;
         timer.setProgress(time);
         t.setText("Tap as fast as you can in 15 seconds");
         t.setTextSize(20);
         score.setText("");
+        pauseBut.setText("PAUSE");
+        isPaused = false;
+    }
+
+    public void pauseButton(View view) {
+        if (time < 30 && time>0) {
+            countDown.cancel();
+            if (isPaused == false) {
+                isPaused = true;
+                pauseBut.setText("UNPAUSE");
+                return;
+            }
+            if (isPaused == true) {
+                isPaused = false;
+                pauseBut.setText("PAUSE");
+                timeActivation();
+                return;
+            }
+        }
     }
 }
